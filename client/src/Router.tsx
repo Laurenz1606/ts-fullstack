aa0677b18138c160f76d5ef54265ca3196b3f0b5
Laurenz1606 <77924509+Laurenz1606@users.noreply.github.com>
2022-02-20 19:59:57 +0100

    Update Router.tsx

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-type Props = {};
-
-export default function Router({}: Props) {
+export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
